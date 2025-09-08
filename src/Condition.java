7e7895738ff94c8bbe7117fb52bce2321d716bab
Ramanand Kumar <rk1074501@gmail.com>
2025-09-08 11:25:44 +0530

    learnt if-else with multiple conditions

--- src/Condition.java
+++ src/Condition.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Condition {
     public static void main(String[] args){
 
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Your age : ");
+        int age = input.nextInt();
+        if (age < 0){
+            System.out.println("Invalid age");
+        }
+        else if (age < 18) {
+            System.out.println("You are underaged");
+            
+        }else if( age <= 60){
+            System.out.println("You are eligible for working");
+
+        }else if (age > 60){
+            System.out.println("You are senior citizen");
+        }
+
+        input.close();
     };
 
 }
